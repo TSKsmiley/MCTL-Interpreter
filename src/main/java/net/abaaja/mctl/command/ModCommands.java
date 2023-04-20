84869e698835dd5a064e36e877459cecf5e4b994
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2023-04-20 15:59:39 +0200

    fs is bing :(

--- src/main/java/net/abaaja/mctl/command/ModCommands.java
+++ src/main/java/net/abaaja/mctl/command/ModCommands.java
@@ -3,6 +3,7 @@ package net.abaaja.mctl.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import net.abaaja.mctl.MCTL;
+import net.abaaja.mctl.command.commands.LoadFileCommand;
 import net.abaaja.mctl.command.commands.TestCommand;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 
 public class ModCommands {
     public static void register(final CommandDispatcher<CommandSourceStack> dispatcher){
+        new LoadFileCommand(dispatcher);
         new TestCommand(dispatcher);
     }
 }
