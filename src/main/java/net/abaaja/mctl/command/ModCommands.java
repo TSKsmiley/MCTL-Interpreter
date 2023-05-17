e96054d70a33f61af4511bbd8498aaf83a33ff42
Anton <dev@atjn.dk>
2023-05-17 16:25:26 +0200

    Add file format command

--- src/main/java/net/abaaja/mctl/command/ModCommands.java
+++ src/main/java/net/abaaja/mctl/command/ModCommands.java
@@ -1,13 +1,13 @@
 package net.abaaja.mctl.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import net.abaaja.mctl.command.commands.EditFileCommand;
-import net.abaaja.mctl.command.commands.LoadFileCommand;
+import net.abaaja.mctl.command.commands.*;
 import net.minecraft.commands.CommandSourceStack;
 
 public class ModCommands {
     public static void register(final CommandDispatcher<CommandSourceStack> dispatcher){
         new LoadFileCommand(dispatcher);
         new EditFileCommand(dispatcher);
+        new FormatFileCommand(dispatcher);
     }
 }
